f26bde61d49ac880b6f3d6566e8108431ef48d30
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-03-31 16:55:02 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def find_min_value(array)
-  # Add your solution here
+  i = 0 
+  minimum_value = 0         
+  while i < array.length do
+    if minimum_value == array[i] 
+      minimum_value = array[i]
+    end 
+  i += 1 
+end
+minimum_value
 end
